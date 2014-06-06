cb114fdbff3e6b3e7d6da7f4368466ffa2f6877f
abevieiramota <abevieiamota@gmail.com>
2014-06-05 23:42:28 -0300

    adicionado slides

--- util.py
+++ util.py
@@ -8,18 +8,30 @@ PLOT_LEN = 3
 FONT_SIZE = 25
 CMAP = 'gray_r'
 
+def show_digits_plots(digitos, ini, fim):
+    
+    num_digits = fim - ini + 1
+    num_rows = int(ceil((1.*num_digits)/NUM_COLS))
+    
+    fig, axes = pl.subplots(num_rows, NUM_COLS, figsize=(PLOT_LEN*NUM_COLS, PLOT_LEN*num_rows))
+
+    for ax, i in zip(axes.ravel(), xrange(ini, fim+1)):
+
+        ax.plot(xrange(len(digitos.data[i])), digitos.data[i])
+        ax.set_title("%d" % digitos.target[i])
+
 def show_digits(digitos, ini, fim):
     
     num_digits = fim - ini + 1
     num_rows = int(ceil((1.*num_digits)/NUM_COLS))
     
-    fig, axis = pl.subplots(num_rows, NUM_COLS, figsize=(PLOT_LEN*NUM_COLS, PLOT_LEN*num_rows))
+    fig, axes = pl.subplots(num_rows, NUM_COLS, figsize=(PLOT_LEN*NUM_COLS, PLOT_LEN*num_rows))
     
-    for ax in axis.ravel():
+    for ax in axes.ravel():
         
         ax.axis('off')
     
-    for ax, i in zip(axis.ravel(), xrange(ini, fim+1)):
+    for ax, i in zip(axes.ravel(), xrange(ini, fim+1)):
         
         # no lugar de data[i].reshape((8,8)) pode-se utilizar images[i]
         ax.imshow(digitos.data[i].reshape((8, 8)), cmap=CMAP, interpolation='nearest')
